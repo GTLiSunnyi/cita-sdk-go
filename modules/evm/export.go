a5f824e6b7279102a5bcd6f25aa4a3111211c85b
GTLiSunnyi <1572839321@qq.com>
2022-10-24 13:00:28 +0800

    add evm modules

--- modules/evm/export.go
+++ modules/evm/export.go
@@ -1,7 +1,11 @@
 package evm
 
-import "github.com/GTLiSunnyi/cita-sdk-go/types"
+import (
+	"github.com/GTLiSunnyi/cita-sdk-go/types"
+	"github.com/GTLiSunnyi/cita-sdk-go/types/contract"
+)
 
 type Client interface {
+	NewContract(header types.GrpcRequestHeader, contractAddress string) (*contract.Contract, error)
 	GetAbi(header types.GrpcRequestHeader, contractAddress string) ([]byte, error)
 }
